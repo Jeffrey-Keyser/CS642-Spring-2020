3b85721004af3b3f62f0f0e3873e0b79e263271e
Shawn Zhong <github@shawnzhong.com>
2020-02-05 12:29:42 -0600

    Update cracking_extra.py

--- HW1/cracking_extra.py
+++ HW1/cracking_extra.py
@@ -1,7 +1,8 @@
 import hashlib
-import itertools
+import os
+import sys
+
 import multiprocessing as mp
-import tqdm
 
 
 def hash(m):
@@ -14,20 +15,37 @@ def hash(m):
 expected = "5483d76bc214a60e35a8a068a28912c168ea5aea8d1441559e3568135185d636"
 assert hash("ironman,password,84829348943") == expected
 
-print(hash("bucky,M@dis0n,8934029034"))
+filename = "crackstation.txt"
+file_size = os.path.getsize(filename)
 
+def try_password(start, size):
+    with open(filename, errors="ignore") as f:
+        f.seek(start)
+        print(start / file_size)
+        sys.stdout.flush()
+        for p in f.readlines(size):
+            if hash(f"bucky,{p},8934029034") == "1b2ebfab6e70dcb13f3ff4750d065bab8474dac4dc611df339446071ae3e7977":
+                print(f"====================== {p} ==================")
 
-# expected = "1b2ebfab6e70dcb13f3ff4750d065bab8474dac4dc611df339446071ae3e7977"
+def chunkify(size=1 << 20):
+    with open(filename, errors="ignore") as f:
+        end = f.tell()
+        while True:
+            start = end
+            f.seek(start + size)
+            f.readline()
+            end = f.tell()
+            yield start, end - start
+            if end > file_size:
+                break
 
+pool = mp.Pool()
+jobs = []
 
-def try_password(p):
-    if hash(f"bucky,{p},8934029034") == expected:
-        return p
+for start, size in chunkify():
+    jobs.append(pool.apply_async(try_password,(start,size)) )
 
-# # Downloaded from https://crackstation.net/crackstation-wordlist-password-cracking-dictionary.htm
-with mp.Pool() as p, open("crackstation-human-only.txt", errors="ignore") as f:
-    for idx, password in enumerate(p.imap_unordered(try_password, f)):
-        if idx % 1000 == 0:
-            print(idx / 63941069)
-        if password:
-            print(password)
+for job in jobs:
+    pass
+pool.close()
+pool.join()
