65575f81028d9ba28c494c5de14f732dc86a9501
Shawn Zhong <github@shawnzhong.com>
2020-02-05 23:31:17 -0600

    Update cracking_extra.py

--- HW1/cracking_extra.py
+++ HW1/cracking_extra.py
@@ -7,7 +7,6 @@
 
 import hashlib
 import os
-import sys
 import tqdm
 
 import multiprocessing as mp
@@ -20,25 +19,17 @@ def hash(m):
     return h.hex()
 
 
-expected = "5483d76bc214a60e35a8a068a28912c168ea5aea8d1441559e3568135185d636"
-assert hash("ironman,password,84829348943") == expected
-
-filename = "crackstation.txt"
-file_size = os.path.getsize(filename)
-
-
-def try_password(start, size):
+def crack(filename, start, size):
+    expected = "1b2ebfab6e70dcb13f3ff4750d065bab8474dac4dc611df339446071ae3e7977"
     with open(filename, errors="ignore") as f:
         f.seek(start)
         for p in f.readlines(size):
-            if (
-                hash(f"bucky,{p},8934029034")
-                == "1b2ebfab6e70dcb13f3ff4750d065bab8474dac4dc611df339446071ae3e7977"
-            ):
+            if hash(f"bucky,{p},8934029034") == expected:
                 print(f"====================== {p} ==================")
 
 
-def chunkify(size):
+def chunkify(filename, size):
+    file_size = os.path.getsize(filename)
     with open(filename, errors="ignore") as f:
         end = f.tell()
         while True:
@@ -51,19 +42,18 @@ def chunkify(size):
                 break
 
 
-pool = mp.Pool(1)
-jobs = []
-chunks = list(chunkify(size=64 << 10))
+expected = "5483d76bc214a60e35a8a068a28912c168ea5aea8d1441559e3568135185d636"
+assert hash("ironman,password,84829348943") == expected
+
+filename = "crackstation-human-only.txt"
+chunks = list(chunkify(filename, size=32 << 10))
 pbar = tqdm.tqdm(total=len(chunks))
+update = lambda _: pbar.update(1)
 
+with mp.Pool() as pool:
+    for start, size in chunks:
+        pool.apply_async(crack, (filename, start, size), callback=update)
 
-def log_result(result):
-    pbar.update(1)
-
-
-for start, size in chunks:
-    jobs.append(pool.apply_async(try_password, (start, size), callback=log_result))
-
-pool.close()
-pool.join()
+    pool.close()
+    pool.join()
 
