68833c993da3571c5d8c9e871df1f09fc14043f9
Shawn Zhong <github@shawnzhong.com>
2020-02-06 00:15:25 -0600

    Update badencrypt.py

--- HW1/badencrypt.py
+++ HW1/badencrypt.py
@@ -10,11 +10,10 @@
 # usage: python3 badencrypt.py testkeyfile
 #
 
-import hashlib
-import os
 import sys
-
+import os
 import Crypto.Cipher.AES
+import hashlib
 
 f = open(sys.argv[1], 'r')
 key = f.readline()
@@ -22,13 +21,13 @@ key = bytes.fromhex(key[:32])
 f.close()
 
 message = \
-    """AMOUNT: $  10.00
-    Originating Acct Holder: Bucky
-    Orgininating Acct #82123-09837
-    
-    I authorized the above amount to be transferred to the account #38108-443280
-    held by a Wisc student at the National Bank of the Cayman Islands.
-    """
+"""AMOUNT: $  10.00
+Originating Acct Holder: Bucky
+Orgininating Acct #82123-09837
+
+I authorized the above amount to be transferred to the account #38108-443280
+held by a Wisc student at the National Bank of the Cayman Islands.
+"""
 
 iv = os.urandom(16)
 cipher = Crypto.Cipher.AES.new(key, Crypto.Cipher.AES.MODE_CBC, IV=iv)
